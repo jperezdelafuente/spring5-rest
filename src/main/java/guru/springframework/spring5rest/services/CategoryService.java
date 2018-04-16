e0620e7b809e50f18b894ad19917d93b948f27a4
jperezdelafuente <jesusperez82@gmail.com>
2018-04-16 23:18:19 +0200

    fix add Category Service

---
+++ src/main/java/guru/springframework/spring5rest/services/CategoryService.java
@@ -0,0 +1,12 @@
+package guru.springframework.spring5rest.services;
+
+import guru.springframework.spring5rest.api.v1.model.CategoryDTO;
+
+import java.util.List;
+
+public interface CategoryService {
+
+    List<CategoryDTO> getAllCategories();
+
+    CategoryDTO getCategoryByName(String name);
+}
