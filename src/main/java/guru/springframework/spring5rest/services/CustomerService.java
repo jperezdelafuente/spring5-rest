867cd0047a8e7b5dd32460358ed9e716f0783638
jperezdelafuente <jesusperez82@gmail.com>
2018-04-20 19:55:19 +0200

    adding customer service DTO and mapping

---
+++ src/main/java/guru/springframework/spring5rest/services/CustomerService.java
@@ -0,0 +1,12 @@
+package guru.springframework.spring5rest.services;
+
+import guru.springframework.spring5rest.api.v1.model.CustomerDTO;
+
+import java.util.List;
+
+public interface CustomerService {
+
+    List<CustomerDTO> getAllCustomers();
+
+    CustomerDTO getCustomerById(Long id);
+}
